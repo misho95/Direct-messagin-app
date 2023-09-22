5935ee4fd7c849119508e06c16efd6f9852c2219
Misho <chapidze.misho95@gmail.com>
2023-09-22 20:29:21 +0400

    fxd

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,4 +1,9 @@
-const InputComponent = ({ type, placeHolder }) => {
+interface PropsType {
+  type: string;
+  placeHolder: string;
+}
+
+const InputComponent = ({ type, placeHolder }: PropsType) => {
   return (
     <fieldset className="border-px1 border-neutral-300 p-1 rounded-md">
       <legend className="px-2 mx-2 text-neutral-500">UserName</legend>
